d20006b4fce3e697d82dfebbe5a8463a42998df9
Yallapu <yellapusony229@gmail.com>
2020-04-15 13:13:54 +0530

    Sample development for SAML

---
+++ simple-identity-provider/src/main/java/com/zensar/cio/BeanConfig.java
@@ -0,0 +1,34 @@
+package com.zensar.cio;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.saml.provider.SamlServerConfiguration;
+import org.springframework.security.saml.provider.identity.config.SamlIdentityProviderServerBeanConfiguration;
+
+@Configuration
+public class BeanConfig extends SamlIdentityProviderServerBeanConfiguration {
+	private final AppConfig config;
+
+	public BeanConfig(AppConfig config) {
+		this.config = config;
+	}
+
+	@Override
+	protected SamlServerConfiguration getDefaultHostSamlServerConfiguration() {
+		return config;
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails userDetails = User.withDefaultPasswordEncoder()
+			.username("user")
+			.password("password")
+			.roles("USER")
+			.build();
+		return new InMemoryUserDetailsManager(userDetails);
+	}
+}
---
+++ simple-identity-provider/src/main/java/com/zensar/cio/SimpleIdentityProviderApplication.java
@@ -0,0 +1,13 @@
+package com.zensar.cio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleIdentityProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleIdentityProviderApplication.class, args);
+	}
+
+}
